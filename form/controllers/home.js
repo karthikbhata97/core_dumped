aa4c3c094f482bfc1bc37b288f2cc3ba778d0970
khamar <khamarali123@gmail.com>
2017-09-08 03:11:07 +0530

    date added

--- form/controllers/home.js
+++ form/controllers/home.js
@@ -4,11 +4,13 @@ app.controller("homeController", function($scope, $http, $resource, $route , Fil
   $scope.main = "Home"
 
   $scope.add_record = function(add_record) {
+
     $scope.record = add_record;
+    console.log($scope.record);
     $scope.record.filename=FileName.getname();
     uploadAPI.upload($scope.myFile, $scope.record).then(function(data) {
       if(data.data.success) {
-        $scope.newrecord = {}
+        // $scope.newrecord = {}
         alert("success")
       }
       else {
