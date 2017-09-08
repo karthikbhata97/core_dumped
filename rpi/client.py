0124c0776c3bbb76c7e935059604ac0cdb02ba68
Karthik <karthik.bhata7@gmail.com>
2017-09-09 04:17:07 +0530

    rpi code working python3

--- rpi/client.py
+++ rpi/client.py
@@ -3,38 +3,40 @@ import numpy as np
 import datetime as dt
 import requests, sys, json, urllib
 
-server = 'http://localhost:3000'
+server = 'http://13.58.17.190'
 headers = {'Content-Type': 'application/json'}
 payload = {'devicename': 'admin', 'password': 'admin'}
+print 'begin'
 
 def getid():
+    print 'getid'
     r = requests.post(server + '/getdata', data = json.dumps(payload), headers = headers)
     if r.status_code != 200:
-        print ("Failed")
+        print "Failed"
         sys.exit(0)
     else:
         res_json = r.json()
         if len(res_json) == 1:
             return res_json[0]["_id"]
         else:
-            print ("Nothing to show")
+            print "Nothing to show"
             return None
 
 def save(img_id):
+    print 'save'
     if img_id:
         link = '/attachments/' + img_id + '.png'
         print (link)
         try:
-            img = urllib.request.urlopen(server + link)
-            with open('display.png', 'b+w') as f:
-                f.write(img.read())
+            urllib.urlretrieve(server + link, 'display.png')
         except:
-            print ("Failed to download")
+            print "Failed to download"
 
 #img = cv2.imread('test_img.jpg')
 current_id = -1
 cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.cv.CV_WINDOW_FULLSCREEN)
+print 'loop begin'
 while(True):
     t = dt.datetime.now()
     img_id = getid()
@@ -47,3 +49,4 @@ while(True):
 
 if cv2.waitKey(0) == 27:
 	cv2.destroyAllWindows()
+
